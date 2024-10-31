9118a3f1de8eb02be975707bb45199135e9ab9ae
reka2002 <88077512+reka2002@users.noreply.github.com>
2024-10-31 11:22:06 +0000

    Update open.py

--- open.py
+++ open.py
@@ -1,8 +1,10 @@
+# https://www.geeksforgeeks.org/understanding-python-pickling-example/
+
 import pickle
+from ada.scheduler.mip_scheduler import *
 
 mypath = "agent_MPC_with_6prods2.pkl"
 #mypath= "C:\\Users\Reka\Documents\GitHub\public_drl_sc\agent_MPC_with_6prods2.pkl"
 with open(mypath, 'rb') as f:
     data = pickle.load(f)
 
-print(data)
